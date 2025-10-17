f8f1c156f9b4fa420c163545d04c520e1ddddd23
eibit <ebitgeek@gmail.com>
2025-10-18 06:47:20 +0800

    update

--- src/components/StickyNote.tsx
+++ src/components/StickyNote.tsx
@@ -10,7 +10,7 @@ export default function StickyNote() {
       <Card className="w-full flex flex-col"> {/* ✅ 给 Card 固定高度 */}
         <CardHeader>
           <CardTitle>便签</CardTitle>
-          <CardDescription>快捷记录一下文本信息</CardDescription>
+          <CardDescription>快捷记录一些文本信息</CardDescription>
         </CardHeader>
 
         {/* ✅ 让内容部分自适应剩余空间 */}
@@ -18,6 +18,7 @@ export default function StickyNote() {
           <Label>便签内容</Label>
           <Textarea
             wrap="off"
+            placeholder="请输入要记录的内容..."
             className="flex-1 w-full resize-none"
             value={stickyNoteText}
             onChange={(e) => setStickyNoteText(e.target.value)}
